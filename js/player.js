81c6ac6dcd3ca616b410be33e7140d0f4ec240f6
Remigius001 <remigiusmbam@gmail.com>
2026-08-12 07:21:43 -0700

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,13 +1,11 @@
 import * as THREE from "three";
-import { GLTFLoader } from
-    "https://cdn.jsdelivr.net/npm/three@0.180.0/examples/jsm/loaders/GLTFLoader.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.180.0/examples/jsm/loaders/GLTFLoader.js";
 
 export class Player {
 
     constructor(scene) {
 
         this.scene = scene;
-
         this.group = new THREE.Group();
 
         this.speed = 0.12;
@@ -16,9 +14,9 @@ export class Player {
         this.velocityY = 0;
         this.gravity = -0.02;
         this.jumpPower = 0.35;
-
         this.isGrounded = true;
 
+        this.model = null;
         this.mixer = null;
 
         scene.add(this.group);
@@ -31,66 +29,40 @@ export class Player {
         const loader = new GLTFLoader();
 
         loader.load(
-            "./assets/characters/logan_blingz_original.glb",
+            "./assets/logan_blingz_original (3).glb",
 
             (gltf) => {
 
-                console.log("LOGAN MODEL LOADED");
+                console.log("Logan GLB loaded!");
 
                 const model = gltf.scene;
 
                 this.model = model;
 
-                // Make every part of the model visible
-                model.traverse((object) => {
-
-                    if (object.isMesh) {
-
-                        object.visible = true;
-
-                        object.castShadow = true;
-                        object.receiveShadow = true;
-                    }
-
-                });
-
-                // Size
                 model.scale.set(
                     0.8,
                     0.8,
                     0.8
                 );
 
-                // Position
                 model.position.set(
                     0,
                     0,
                     0
                 );
 
+                model.traverse((object) => {
+
+                    if (object.isMesh) {
+
+                        object.visible = true;
+                        object.castShadow = true;
+                        object.receiveShadow = true;
+                    }
+                });
+
                 this.group.add(model);
 
-                // Center the model
-                const box =
-                    new THREE.Box3()
-                        .setFromObject(model);
-
-                const center =
-                    box.getCenter(
-                        new THREE.Vector3()
-                    );
-
-                model.position.x -= center.x;
-                model.position.z -= center.z;
-
-                // Put feet on the ground
-                const finalBox =
-                    new THREE.Box3()
-                        .setFromObject(model);
-
-                model.position.y -= finalBox.min.y;
-
-                // Animations, if the model has them
                 if (gltf.animations.length > 0) {
 
                     this.mixer =
@@ -103,7 +75,6 @@ export class Player {
 
                     action.play();
                 }
-
             },
 
             undefined,
@@ -111,7 +82,7 @@ export class Player {
             (error) => {
 
                 console.error(
-                    "Could not load Logan GLB:",
+                    "Logan GLB error:",
                     error
                 );
             }
@@ -142,39 +113,24 @@ export class Player {
             this.group.position.x += speed;
         }
 
-        // Jump
-        if (
-            keys[" "] &&
-            this.isGrounded
-        ) {
+        if (keys[" "] && this.isGrounded) {
 
-            this.velocityY =
-                this.jumpPower;
-
+            this.velocityY = this.jumpPower;
             this.isGrounded = false;
         }
 
-        // Gravity
         this.velocityY += this.gravity;
 
-        this.group.position.y +=
-            this.velocityY;
+        this.group.position.y += this.velocityY;
 
-        // Ground collision
-        if (
-            this.group.position.y <= 0
-        ) {
+        if (this.group.position.y <= 0) {
 
             this.group.position.y = 0;
-
             this.velocityY = 0;
-
             this.isGrounded = true;
         }
 
-        // Animation
         if (this.mixer) {
-
             this.mixer.update(delta);
         }
     }
