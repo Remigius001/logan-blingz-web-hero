9ca8ab86f7f68a6f6d7d42bbf343b14d17ba14e9
Remigius001 <remigiusmbam@gmail.com>
2026-08-12 03:58:25 -0700

    Update player.js

--- .github/player.js
+++ .github/player.js
@@ -4,15 +4,6 @@ export class Player {
 
     constructor(scene) {
         this.scene = scene;
-
-        this.group = new THREE.Group();
-
-        this.speed = 0.12;
-        this.jumpPower = 0.35;
-        this.velocityY = 0;
-        this.isGrounded = true;
-
-        scene.add(this.group);
     }
 
 }
