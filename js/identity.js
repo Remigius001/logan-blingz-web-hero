7137eb387f080aeaf92923c043cee2ce07787e75
Remigius001 <remigiusmbam@gmail.com>
2026-08-14 05:40:44 -0700

    Implement IdentitySystem class with identity management

--- js/identity.js
+++ js/identity.js
@@ -1,1 +1,80 @@
-// This system will be built later.
+export class IdentitySystem {
+
+    constructor(player) {
+
+        this.player = player;
+
+        this.normalIdentity = {
+            name: "Logan",
+            role: "Student"
+        };
+
+        this.heroIdentity = {
+            name: "Logan Blingz",
+            role: "Hero"
+        };
+
+        this.isHero = false;
+
+        this.secretIdentitySafe = true;
+    }
+
+    getCurrentIdentity() {
+
+        if (this.isHero) {
+            return this.heroIdentity;
+        }
+
+        return this.normalIdentity;
+    }
+
+    transformToHero() {
+
+        if (this.isHero) {
+            return;
+        }
+
+        this.isHero = true;
+
+        console.log(
+            "Logan changed into his hero identity."
+        );
+    }
+
+    transformToNormal() {
+
+        if (!this.isHero) {
+            return;
+        }
+
+        this.isHero = false;
+
+        console.log(
+            "Logan returned to his normal identity."
+        );
+    }
+
+    toggleIdentity() {
+
+        if (this.isHero) {
+            this.transformToNormal();
+        } else {
+            this.transformToHero();
+        }
+    }
+
+    getName() {
+
+        return this.getCurrentIdentity().name;
+    }
+
+    getRole() {
+
+        return this.getCurrentIdentity().role;
+    }
+
+    isSecretIdentitySafe() {
+
+        return this.secretIdentitySafe;
+    }
+}
